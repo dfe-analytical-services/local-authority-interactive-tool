cb7ef92e4176cf063880bce99bdb43e6653ac580
Jake Tufts <jake.tufts@education.gov.uk>
2024-08-22 11:23:35 +0100

    Updating renv, removing library calls and adding styling packages to false load

--- global.R
+++ global.R
@@ -13,40 +13,22 @@ shhh <- suppressPackageStartupMessages # It's a library, so shhh!
 
 # Core shiny and R packages
 shhh(library(shiny))
-# shhh(library(bslib))
-# shhh(library(rstudioapi))
-
-# Custom packages
-# shhh(library(dfeR))
-# shhh(library(dfeshiny))
-# shhh(library(shinyGovstyle))
 
 # Creating charts and tables
 shhh(library(ggplot2))
 shhh(library(ggiraph))
-# shhh(library(DT))
-
-# Data and string manipulation
-# shhh(library(dplyr))
-# shhh(library(stringr))
-
-# Shiny extensions
-# shhh(library(shinyjs))
-# shhh(library(tools))
-# shhh(library(shinydashboard))
-# shhh(library(shinyWidgets))
-# shhh(library(shinytitle))
-# shhh(library(xfun))
-# shhh(library(metathis))
-# shhh(library(shinyalert))
 
 # Dependencies needed for testing or CI but not for the app -------------------
 # Including them here keeps them in renv but avoids the app needlessly loading
 # them, saving on load time.
 if (FALSE) {
-  shhh(library(shinytest2))
+  # Testing
   shhh(library(testthat))
+  shhh(library(shinytest2))
+  # Styling
+  shhh(library(rstudioapi))
   shhh(library(lintr))
+  shhh(library(styler))
 }
 
 # Source scripts --------------------------------------------------------------
