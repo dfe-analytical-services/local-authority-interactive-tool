5f52aaec73f2f50dcaaf3204c438849e01017f71
Jake Tufts <jake.tufts@education.gov.uk>
2024-09-03 12:23:09 +0100

    Test: Added UI test using minimal app.R for mod_la_lvl_table

--- tests/testthat/test-UI-mod_la_lvl_table.R
+++ tests/testthat/test-UI-mod_la_lvl_table.R
@@ -1,3 +1,4 @@
+# 1. Uses root app for setup --------------------------------------------------
 # Load global
 source(here::here("global.R"))
 
@@ -25,44 +26,26 @@ testthat::test_that("There are 4 rows in the LA main table", {
   testthat::expect_true(nrow(la_table_df) == 4)
 })
 
+shinytest_app$stop()
 
 
-# # Load global
-# source(here::here("global.R"))
-#
-# # Create minimal app
-# minimal_ui <- shiny::fluidRow(
-#   title = "Minimal app",
-#
-#   # User inputs
-#   appInputsUI("la_level"),
-#
-#   # Main table
-#   LA_LevelTableUI("la_table")
-# )
-#
-# minimal_server <- function(input, output, session) {
-#   # User Inputs
-#   app_inputs <- appInputsServer("la_level")
-#
-#   # Main table
-#   la_main_tbl <- LA_LevelTableServer(
-#     "la_table",
-#     app_inputs,
-#     bds_metrics,
-#     stat_n_la
-#   )
-#
-#   # Export values for use in UI tests
-#   exportTestValues(
-#     la_main_tbl = la_main_tbl()
-#   )
-# }
-#
-# minimal_app <- shinyApp(minimal_ui, minimal_server)
-#
-# shinytest_app <- shinytest2::AppDriver$new(minimal_app)
-#
-# la_main_tbl <- app$get_values(export = c("la_main_tbl"))
-#
-# rev_bal_export$export$avg_rev_bal_value
+# 2. Uses minimal test-app .R file for setup ----------------------------------
+shinytest_app <- shinytest2::AppDriver$new(
+  app_dir = here::here("tests/testthat/test-app_mod_la_lvl_table/"),
+  load_timeout = 45 * 1000
+)
+
+# Get export values
+la_main_tbl <- shinytest_app$get_values(export = c("la_main_tbl"))
+
+# Convert reactable JSON to dataframe
+la_main_tbl_list <- jsonlite::fromJSON(la_main_tbl$export$la_main_tbl)
+la_table_data <- la_main_tbl_list$x$tag$attribs$data
+la_table_df <- as.data.frame(la_table_data, check.names = FALSE)
+
+# Test
+testthat::test_that("There are 4 rows in the LA main table", {
+  testthat::expect_true(nrow(la_table_df) == 4)
+})
+
+shinytest_app$stop()
---
+++ tests/testthat/test-app_mod_la_lvl_table/app.R
@@ -0,0 +1,34 @@
+# Load global
+shinytest2::load_app_env(app_dir = here::here())
+source(here::here("global.R"))
+
+# Create minimal app
+minimal_ui <- shiny::fluidRow(
+  title = "Minimal app",
+
+  # User inputs
+  appInputsUI("la_level"),
+
+  # Main table
+  LA_LevelTableUI("la_table")
+)
+
+minimal_server <- function(input, output, session) {
+  # User Inputs
+  app_inputs <- appInputsServer("la_level")
+
+  # Main table
+  la_main_tbl <- LA_LevelTableServer(
+    "la_table",
+    app_inputs,
+    bds_metrics,
+    stat_n_la
+  )
+
+  # Export values for use in UI tests
+  exportTestValues(
+    la_main_tbl = la_main_tbl()
+  )
+}
+
+shinyApp(minimal_ui, minimal_server)
