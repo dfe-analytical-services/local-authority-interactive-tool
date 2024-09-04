56231a5efbb8c8888957e345a59ac36768351c46
Jake Tufts <jake.tufts@education.gov.uk>
2024-09-04 17:02:52 +0100

    Tests: New server test for LA line chart

--- tests/testthat/test-Server-mod_la_lvl_charts.R
+++ tests/testthat/test-Server-mod_la_lvl_charts.R
@@ -1,3 +1,36 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+testthat::test_that("LA_LineChartServer creates a line chart with correct data", {
+  # Mocking app_inputs and other required data
+  app_inputs <- shiny::reactiveValues(
+    la = reactive({
+      "Barking and Dagenham"
+    }),
+    topic = reactive({
+      "Economic Factors"
+    }),
+    indicator = reactive({
+      "Percentage of children in low income families"
+    })
+  )
+
+  # Running the test server
+  shiny::testServer(LA_LineChartServer, args = list(app_inputs, bds_metrics, stat_n_la), {
+    # Trigger reactivity to simulate the app environment
+    session$flushReact()
+
+    # Retrieve the output plot
+    output_plot <- la_line_chart()
+
+    # Check that the output is a ggiraph object
+    testthat::expect_true(inherits(output_plot, "girafe"))
+
+    # Check that the plot contains the expected data
+    plot_data <- output$line_chart
+    plot_data_list <- jsonlite::fromJSON(plot_data)
+
+    # Find chart title?
+    # Check title
+    testthat::expect_true(
+      grepl("Percentage of children in low income families", plot_data_list$x$html)
+    )
+  })
 })
