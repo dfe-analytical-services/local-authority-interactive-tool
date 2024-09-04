b4079186b71cbff2850e1a36fcb67e236ed787eb
Jake Tufts <jake.tufts@education.gov.uk>
2024-09-04 16:18:53 +0100

    Test: Added test files for other LA modules, charts and metadata. Moved the hyperlink test into the metadata test file

---
+++ tests/testthat/test-Server-mod_la_lvl_metadata.R
@@ -0,0 +1,16 @@
+# LA_LevelMetaServer ----------------------------------------------------------
+test_that("1. LA_LevelMetaServer correctly displays hyperlink", {
+  # Simulate the app_inputs object with reactive values
+  indicator_input <- shiny::reactive({
+    "First Time entrants to the Youth Justice System"
+  })
+
+  shiny::testServer(LA_LevelMetaServer, args = list(indicator_input, metrics_clean), {
+    # Get the source output
+    source_output <- output$source
+
+    # Test that the output is a link
+    expect_true(grepl("<a", source_output$html))
+    expect_true(grepl("http", source_output$html))
+  })
+})
