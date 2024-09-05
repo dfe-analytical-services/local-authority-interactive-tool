ece6e4be492008f1c7a4cc938271190b62a98788
Jake Tufts <jake.tufts@education.gov.uk>
2024-09-05 17:03:24 +0100

    Feat: Added Regions table and Stats table to Region page dev app

--- 02_dev/regional_page_features_workshop.R
+++ 02_dev/regional_page_features_workshop.R
@@ -34,13 +34,13 @@ filtered_bds <- bds_metrics |>
     Measure == selected_indicator
   )
 
-# Then filter for selected LA, region, stat neighbours and relevant national
+# Then filter for selected LA and regional LAs
 region_la_filtered_bds <- filtered_bds |>
   dplyr::filter(
     `LA and Regions` %in% c(selected_la, region_la_la)
   )
 
-# LA levels long
+# Region LA levels long
 region_la_long <- region_la_filtered_bds |>
   # dplyr::filter(`LA and Regions` %notin% c(la_sns)) |>
   dplyr::select(`LA Number`, `LA and Regions`, Years, values_num) |>
@@ -65,8 +65,8 @@ current_year <- region_la_long |>
   dplyr::filter(Years_num == max(Years_num)) |>
   pull_uniques("Years")
 
-# Join difference and pivot wider to recreate LAIT table
-region_la_able <- region_la_long |>
+# Join difference and pivot wider to recreate Regional LA table
+region_la_table <- region_la_long |>
   dplyr::bind_rows(region_la_diff) |>
   tidyr::pivot_wider(
     id_cols = c("LA Number", "LA and Regions"),
@@ -74,7 +74,104 @@ region_la_able <- region_la_long |>
     values_from = values_num
   ) |>
   pretty_num_table(dp = 1) |>
-  dplyr::arrange(.data[[current_year]])
+  dplyr::arrange(.data[[current_year]], `LA and Regions`)
 
 
 # Regional Level Regions table ------------------------------------------------
+# Get national term
+region_national <- filtered_bds |>
+  dplyr::filter(`LA and Regions` %in% national_names_bds & !is.na(values_num)) |>
+  pull_uniques("LA and Regions")
+
+# Filter for all regions and England
+region_filtered_bds <- filtered_bds |>
+  dplyr::filter(
+    `LA and Regions` %in% c(region_names_bds, region_national)
+  )
+
+# Region levels long
+region_long <- region_filtered_bds |>
+  # dplyr::filter(`LA and Regions` %notin% c(la_sns)) |>
+  dplyr::select(`LA Number`, `LA and Regions`, Years, values_num) |>
+  # dplyr::bind_rows(sn_avg) |>
+  dplyr::mutate(
+    `LA and Regions` = factor(
+      `LA and Regions`
+      # levels = c(
+      #   selected_la, la_region,
+      #   "Statistical Neighbours", la_national
+      # )
+    ),
+    Years_num = as.numeric(substr(Years, start = 1, stop = 4))
+  )
+
+# Difference between last two years
+region_diff <- region_long |>
+  calculate_change_from_prev_yr()
+
+# Most recent year
+current_year <- region_long |>
+  dplyr::filter(Years_num == max(Years_num)) |>
+  pull_uniques("Years")
+
+# Join difference and pivot wider to recreate Region table
+region_table <- region_long |>
+  dplyr::bind_rows(region_diff) |>
+  tidyr::pivot_wider(
+    id_cols = c("LA Number", "LA and Regions"),
+    names_from = Years,
+    values_from = values_num
+  ) |>
+  pretty_num_table(dp = 1) |>
+  dplyr::arrange(.data[[current_year]], `LA and Regions`) |>
+  # Places England row at the bottom of the table
+  dplyr::mutate(is_england = ifelse(grepl("England \\(", `LA and Regions`), 1, 0)) |>
+  dplyr::arrange(is_england, .by_group = FALSE) |>
+  dplyr::select(-is_england)
+
+
+# Regional Level Stats table --------------------------------------------------
+# Get LA numbers
+# Selected LA
+region_la_la_num <- region_la_table |>
+  filter_la_regions(selected_la, pull_col = "LA Number")
+
+# Region and England
+region_la_num <- region_table |>
+  filter_la_regions(c(region_la, region_national), pull_col = "LA Number")
+
+# Get change in previous year
+# Selected LA
+region_la_change_prev <- region_la_table |>
+  filter_la_regions(selected_la,
+    latest = FALSE,
+    pull_col = "Change from previous year"
+  )
+
+# Region and England
+region_change_prev <- region_table |>
+  filter_la_regions(c(region_la, region_national),
+    latest = FALSE,
+    pull_col = "Change from previous year"
+  )
+
+# Creating the stats table cols
+region_stats_la_num <- c(region_la_la_num, region_la_num)
+region_stats_name <- c(selected_la, region_la, region_national)
+region_stats_change <- c(region_la_change_prev, region_change_prev)
+
+# Creating the trend descriptions
+region_trend <- dplyr::case_when(
+  is.na(region_stats_change) ~ NA_character_,
+  region_stats_change > 0 ~ "Increase",
+  region_stats_change < 0 ~ "Decrease",
+  TRUE ~ "No trend"
+)
+
+# Build stats table
+region_stats_table <- data.frame(
+  "LA Number" = region_stats_la_num,
+  "LA and Regions" = region_stats_name,
+  "Trend" = region_trend,
+  "Change from previous year" = region_stats_change
+)
