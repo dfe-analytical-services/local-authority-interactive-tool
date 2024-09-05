adc7aa35113957c18b6326b23e6963db84a1a505
Jake Tufts <jake.tufts@education.gov.uk>
2024-09-05 12:08:25 +0100

    Tests: New Server shinytest for LA bar chart, changed some of the inputs so no dupes

--- tests/testthat/test-Server-mod_la_lvl_charts.R
+++ tests/testthat/test-Server-mod_la_lvl_charts.R
@@ -4,13 +4,13 @@ testthat::test_that("LA_LineChartServer creates a ggiraph chart with the correct
   # Mocking app_inputs and other required data
   app_inputs <- shiny::reactiveValues(
     la = reactive({
-      "Barking and Dagenham"
+      "Cumbria"
     }),
     topic = reactive({
-      "Economic Factors"
+      "Looked After Children"
     }),
     indicator = reactive({
-      "Percentage of children in low income families"
+      "LAC Key Stage 4 - AVG Attainment 8 score"
     })
   )
 
@@ -31,7 +31,46 @@ testthat::test_that("LA_LineChartServer creates a ggiraph chart with the correct
 
     # Check title
     testthat::expect_true(
-      grepl("Percentage of children in low income families", plot_data_list$x$html)
+      grepl("LAC Key Stage 4 - AVG Attainment 8 score - Average Score", plot_data_list$x$html)
+    )
+  })
+})
+
+
+testthat::test_that("LA_BarChartServer creates a ggiraph chart with the correct title", {
+  mute_cat(source(here::here("global.R")))
+
+  # Mocking app_inputs and other required data
+  app_inputs <- shiny::reactiveValues(
+    la = reactive({
+      "Wirral"
+    }),
+    topic = reactive({
+      "Children with SEN"
+    }),
+    indicator = reactive({
+      "Newly issued statements and plans in LA maintained mainstream schools"
+    })
+  )
+
+  # Running the test server
+  shiny::testServer(LA_BarChartServer, args = list(app_inputs, bds_metrics, stat_n_la), {
+    # Trigger reactivity to simulate the app environment
+    session$flushReact()
+
+    # Retrieve the output plot
+    output_plot <- la_bar_chart()
+
+    # Check that the output is a ggiraph object
+    testthat::expect_true(inherits(output_plot, "girafe"))
+
+    # Check that the plot contains the expected data
+    plot_data <- output$bar_chart
+    plot_data_list <- jsonlite::fromJSON(plot_data)
+
+    # Check title
+    testthat::expect_true(
+      grepl("Newly issued statements and plans in LA maintained mainstream schools - % statements/plans", plot_data_list$x$html)
     )
   })
 })
