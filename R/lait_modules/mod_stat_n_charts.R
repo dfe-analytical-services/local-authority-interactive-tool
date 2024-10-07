7df00d58f322ebffe3597d061d290965d6c72b06
Jake Tufts <jake.tufts@education.gov.uk>
2024-10-07 10:15:22 +0100

    Chore: Updating logic of the dependency between chart inputs in Stat neigh plots

--- R/lait_modules/mod_stat_n_charts.R
+++ R/lait_modules/mod_stat_n_charts.R
@@ -389,91 +389,83 @@ StatN_Chart_InputServer <- function(id, la_input, stat_n_long, shared_values) {
       )
     })
 
-    # Observe if bar input becomes empty and clear both shared inputs if it does
-    observeEvent(c(input$chart_bar_input, input$chart_line_input),
-      {
-        if (is.null(input$chart_bar_input) || is.null(input$chart_line_input)) {
-          shared_values$chart_bar_input <- NULL
-          shared_values$chart_line_input <- NULL
-        }
-
-        print("A - Bar & Line")
-      },
-      ignoreNULL = FALSE
-    )
-
-    # Keep the line input synchronized with shared values
-    observeEvent(shared_values$chart_line_input,
-      {
-        updateSelectizeInput(
-          session = session,
-          inputId = "chart_line_input",
-          selected = if (is.null(shared_values$chart_line_input)) character(0) else shared_values$chart_line_input
-        )
-        print("B - Line")
-      },
-      ignoreNULL = FALSE
-    )
-
-    # Keep the bar input synchronized with shared values
-    observeEvent(shared_values$chart_bar_input,
-      {
-        updateSelectizeInput(
-          session = session,
-          inputId = "chart_bar_input",
-          selected = if (is.null(shared_values$chart_bar_input)) character(0) else shared_values$chart_bar_input
-        )
-        print("B - Bar")
-      },
-      ignoreNULL = FALSE
-    )
-
-    # Update shared values when the bar chart input changes
-    observeEvent(input$chart_bar_input,
-      {
-        shared_values$chart_bar_input <- input$chart_bar_input
-        print("C - Bar")
-      },
-      ignoreNULL = FALSE
-    )
-
-    # Update shared values when the line chart input changes
+    # Line chart input --------------------------------------------------------
     observeEvent(input$chart_line_input,
       {
-        shared_values$chart_line_input <- input$chart_line_input
-        print("C - Line")
+        if (!setequal(input$chart_line_input, shared_values$chart_line_input)) {
+          # Update line chart shared val with user input
+          shared_values$chart_line_input <- input$chart_line_input
+
+          # Also update bar chart with line chart input (sync)
+          shared_values$chart_bar_input <- input$chart_line_input
+
+          # print(paste0("Line update: ", input$chart_line_input))
+        }
       },
-      ignoreNULL = FALSE
+      ignoreNULL = FALSE,
+      ignoreInit = TRUE
     )
 
-    # Synchronize the bar input with the line input whenever the bar input changes
-    observeEvent(shared_values$chart_bar_input, {
-      isolate({
-        # Update the line input to reflect the changes in the bar input
-        updateSelectizeInput(
-          session = session,
-          inputId = "chart_line_input",
-          selected = if (is.null(shared_values$chart_bar_input)) character(0) else shared_values$chart_bar_input
-        )
-        shared_values$chart_line_input <- shared_values$chart_bar_input
-      })
-      print("E - Line")
-    })
+    # Keep the bar selected synchronized with shared values
+    observeEvent(shared_values$chart_bar_input,
+      {
+        isolate({
+          if (!setequal(input$chart_bar_input, shared_values$chart_bar_input)) {
+            updateSelectizeInput(
+              session = session,
+              inputId = "chart_bar_input",
+              selected = if (is.null(shared_values$chart_bar_input)) {
+                character(0)
+              } else {
+                shared_values$chart_bar_input
+              }
+            )
+            # print(paste0("Bar change: ", shared_values$chart_bar_input))
+          }
+        })
+      },
+      ignoreNULL = FALSE,
+      ignoreInit = TRUE
+    )
 
-    # Synchronize the line input with the bar input whenever the line input changes
-    observeEvent(shared_values$chart_line_input, {
-      isolate({
-        # Update the bar input to reflect the changes in the line input
-        updateSelectizeInput(
-          session = session,
-          inputId = "chart_bar_input",
-          selected = if (is.null(shared_values$chart_line_input)) character(0) else shared_values$chart_line_input
-        )
+    # Bar chart input ---------------------------------------------------------
+    observeEvent(input$chart_bar_input,
+      {
+        if (!setequal(input$chart_bar_input, shared_values$chart_bar_input)) {
+          # Update bar chart shared val with user input
+          shared_values$chart_bar_input <- input$chart_bar_input
 
-        shared_values$chart_bar_input <- shared_values$chart_line_input
-      })
-      print("E - Bar")
-    })
+          # Also update line chart with bar chart input (sync)
+          shared_values$chart_line_input <- input$chart_bar_input
+
+          # print(paste0("Bar update: ", input$chart_bar_input))
+        }
+      },
+      ignoreNULL = FALSE,
+      ignoreInit = TRUE
+    )
+
+    # Keep the line selected synchronized with shared values
+    observeEvent(shared_values$chart_line_input,
+      {
+        isolate({
+          if (!setequal(input$chart_line_input, shared_values$chart_line_input)) {
+            updateSelectizeInput(
+              session = session,
+              inputId = "chart_line_input",
+              selected = if (is.null(shared_values$chart_line_input)) {
+                character(0)
+              } else {
+                shared_values$chart_line_input
+              }
+            )
+            # print(paste0("Line change: ", shared_values$chart_line_input))
+          }
+        })
+      },
+      ignoreNULL = FALSE,
+      ignoreInit = TRUE
+    )
 
     # Return the selected inputs as reactive values for use elsewhere in the app
     list(
@@ -805,3 +797,321 @@ StatN_MultiBarChartServer <- function(id,
 }
 
 # nolint end
+
+
+
+
+
+
+
+
+
+
+# =============================================================================
+
+StatN_Chart_Input2UI <- function(id) {
+  ns <- NS(id)
+
+  tagList(
+    shiny::selectizeInput(
+      inputId = ns("chart_line_input"),
+      label = "Select region to compare (max 3)",
+      choices = NULL,
+      multiple = TRUE,
+      options = list(
+        maxItems = 3,
+        plugins = list("remove_button"),
+        dropdownParent = "body"
+      )
+    ),
+    shiny::selectizeInput(
+      inputId = ns("chart_bar_input"),
+      label = "Select region to compare (max 3)",
+      choices = NULL,
+      multiple = TRUE,
+      options = list(
+        maxItems = 3,
+        plugins = list("remove_button"),
+        dropdownParent = "body"
+      )
+    )
+  )
+}
+
+
+StatN_Chart_Input2Server <- function(id, la_input, stat_n_long, shared_values) {
+  moduleServer(id, function(input, output, session) {
+    # Helper function to retain only the valid selections that are in the available choices
+    retain_valid_selections <- function(current_choices, previous_selections) {
+      intersect(previous_selections, current_choices)
+    }
+
+    # Reactive expression to get the valid areas (LAs and Regions) excluding the currently selected LA
+    valid_selections <- reactive({
+      stat_n_long() |>
+        dplyr::filter(`LA and Regions` != la_input()) |> # Exclude the selected LA from choices
+        pull_uniques("LA and Regions") # Get the unique values of LA and Regions
+    })
+
+    # Observe when the main LA input changes to update both chart inputs (line and bar)
+    observeEvent(la_input(), {
+      # Get previous selections for both line and bar inputs from shared values
+      prev_line_selections <- input$chart_line_input
+      prev_bar_selections <- input$chart_bar_input
+
+      # Retain only valid selections from the previous inputs
+      valid_line_selections <- retain_valid_selections(valid_selections(), prev_line_selections)
+      valid_bar_selections <- retain_valid_selections(valid_selections(), prev_bar_selections)
+
+      # Update the line chart selectize input with valid selections
+      updateSelectizeInput(
+        session = session,
+        inputId = "chart_line_input",
+        choices = valid_selections(),
+        selected = valid_line_selections
+      )
+
+      # Update the bar chart selectize input with valid selections
+      updateSelectizeInput(
+        session = session,
+        inputId = "chart_bar_input",
+        choices = valid_selections(),
+        selected = valid_bar_selections
+      )
+    })
+
+    # Keep line and bar inputs synchronized without resetting selections
+    observeEvent(input$chart_line_input, {
+      if (!identical(input$chart_bar_input, input$chart_line_input)) {
+        updateSelectInput(
+          session = session,
+          inputId = "chart_bar_input",
+          selected = input$chart_line_input
+        )
+      }
+    })
+
+    observeEvent(input$chart_bar_input, {
+      if (!identical(input$chart_line_input, input$chart_bar_input)) {
+        updateSelectInput(
+          session = session,
+          inputId = "chart_line_input",
+          selected = input$chart_bar_input
+        )
+      }
+    })
+
+    # Return the selected inputs as reactive values for use elsewhere in the app
+    list(
+      line_input = reactive(input$chart_line_input),
+      bar_input = reactive(input$chart_bar_input)
+    )
+  })
+}
+
+
+StatN_MultiChartsUI <- function(id) {
+  ns <- NS(id)
+
+  tagList(
+    bslib::nav_panel(
+      title = "Line chart - user selection",
+      bslib::card(
+        id = "stat_n_multi_line",
+        bslib::card_body(
+          bslib::layout_sidebar(
+            sidebar = bslib::sidebar(
+              title = "Filter options",
+              position = "left",
+              StatN_Chart_Input2UI(
+                ns("chart_input") # Line chart input only
+              )[[1]]
+            ),
+            ggiraph::girafeOutput(ns("line_chart"))
+          )
+        ),
+        full_screen = TRUE
+      )
+    ),
+    bslib::nav_panel(
+      title = "Bar chart - user selection",
+      bslib::card(
+        id = "stat_n_multi_bar",
+        bslib::card_body(
+          bslib::layout_sidebar(
+            sidebar = bslib::sidebar(
+              title = "Filter options",
+              position = "left",
+              StatN_Chart_Input2UI(
+                ns("chart_input") # Line chart input only
+              )[[2]]
+            ),
+            ggiraph::girafeOutput(ns("bar_chart"))
+          )
+        ),
+        full_screen = TRUE
+      )
+    )
+  )
+}
+
+
+StatN_MultiChartsServer <- function(id,
+                                    app_inputs,
+                                    bds_metrics,
+                                    stat_n_la,
+                                    shared_values) {
+  moduleServer(id, function(input, output, session) {
+    # Filter for selected topic and indicator
+    filtered_bds <- BDS_FilteredServer("filtered_bds", app_inputs, bds_metrics)
+
+    # Get Statistical Neighbour long format
+    stat_n_long <- StatN_LongServer(
+      "stat_n_long",
+      app_inputs$la,
+      filtered_bds,
+      stat_n_la
+    )
+
+    # Pulling specific choices available for selected LA & indicator
+    line_input <- StatN_Chart_Input2Server(
+      "chart_input",
+      app_inputs$la,
+      stat_n_long,
+      shared_values
+    )$line_input
+
+    # Pulling specific choices available for selected LA & indicator
+    bar_input <- StatN_Chart_Input2Server(
+      "chart_input",
+      app_inputs$la,
+      stat_n_long,
+      shared_values
+    )$bar_input
+
+
+    # Statistical Neighbour Level SN multi-choice line plot -----------------------
+    output$line_chart <- ggiraph::renderGirafe({
+      # Stores all valid regions in data
+      valid_regions <- stat_n_long()$`LA and Regions`
+
+      # Filter Statistical Neighbour data for these areas
+      stat_n_line_chart_data <- stat_n_long() |>
+        # Filter for random areas - simulate user choosing up to 6 areas
+        dplyr::filter(
+          (`LA and Regions` %in% line_input()) |
+            (`LA and Regions` %in% app_inputs$la())
+        ) |>
+        # Set area orders so selection order starts on top of plot
+        reorder_la_regions(
+          rev(intersect(c(app_inputs$la(), line_input()), valid_regions)),
+          after = Inf
+        )
+
+      # Plot - selected areas
+      multi_line_chart <- stat_n_line_chart_data |>
+        ggplot2::ggplot() +
+        ggiraph::geom_point_interactive(
+          ggplot2::aes(
+            x = Years_num,
+            y = values_num,
+            color = `LA and Regions`,
+            data_id = `LA and Regions`
+          ),
+          na.rm = TRUE
+        ) +
+        ggiraph::geom_line_interactive(
+          ggplot2::aes(
+            x = Years_num,
+            y = values_num,
+            color = `LA and Regions`,
+            data_id = `LA and Regions`
+          ),
+          na.rm = TRUE
+        ) +
+        format_axes(stat_n_line_chart_data) +
+        manual_colour_mapping(
+          c(app_inputs$la(), line_input()),
+          type = "line"
+        ) +
+        set_plot_labs(filtered_bds()) +
+        custom_theme() +
+        # Revert order of the legend so goes from right to left
+        ggplot2::guides(color = ggplot2::guide_legend(reverse = TRUE))
+
+
+      # Creating vertical geoms to make vertical hover tooltip
+      vertical_hover <- lapply(
+        get_years(stat_n_line_chart_data),
+        tooltip_vlines,
+        stat_n_line_chart_data,
+        get_indicator_dps(filtered_bds())
+      )
+
+      # Plotting interactive graph
+      ggiraph::girafe(
+        ggobj = (multi_line_chart + vertical_hover),
+        width_svg = 8,
+        options = generic_ggiraph_options(
+          opts_hover(
+            css = "stroke-dasharray:5,5;stroke:black;stroke-width:2px;"
+          )
+        ),
+        fonts = list(sans = "Arial")
+      )
+    })
+
+
+    # Statistical Neighbour multi-choice bar plot -------------------------------
+    output$bar_chart <- ggiraph::renderGirafe({
+      # Stores all valid regions in data
+      valid_regions <- stat_n_long()$`LA and Regions`
+
+      stat_n_bar_multi_data <- stat_n_long() |>
+        # Filter for random areas - simulate user choosing up to 6 areas
+        dplyr::filter(
+          (`LA and Regions` %in% bar_input()) |
+            (`LA and Regions` %in% app_inputs$la())
+        ) |>
+        # Set area orders so selection order starts on top of plot
+        reorder_la_regions(
+          intersect(c(app_inputs$la(), bar_input()), valid_regions)
+        )
+
+      stat_n_multi_bar_chart <- stat_n_bar_multi_data |>
+        ggplot2::ggplot() +
+        ggiraph::geom_col_interactive(
+          ggplot2::aes(
+            x = Years_num,
+            y = values_num,
+            fill = `LA and Regions`,
+            tooltip = glue::glue_data(
+              stat_n_bar_multi_data |>
+                pretty_num_table(include_columns = "values_num", dp = get_indicator_dps(filtered_bds())),
+              "Year: {Years}\n{`LA and Regions`}: {values_num}"
+            ),
+            data_id = `LA and Regions`
+          ),
+          position = "dodge",
+          width = 0.6,
+          na.rm = TRUE,
+          colour = "black"
+        ) +
+        format_axes(stat_n_bar_multi_data) +
+        manual_colour_mapping(
+          c(app_inputs$la(), bar_input()),
+          type = "bar"
+        ) +
+        set_plot_labs(filtered_bds()) +
+        custom_theme()
+
+      # Plotting interactive graph
+      ggiraph::girafe(
+        ggobj = stat_n_multi_bar_chart,
+        width_svg = 8,
+        options = generic_ggiraph_options(),
+        fonts = list(sans = "Arial")
+      )
+    })
+  })
+}
