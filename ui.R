89d34a42e10127614a3ef6111c2cdbccbc61b5c3
Jake Tufts <jake.tufts@education.gov.uk>
2024-10-17 17:44:26 +0100

    Fix: Remove old JS in ui and updated screenshots for LA charts

--- ui.R
+++ ui.R
@@ -25,9 +25,6 @@ ui <- function(input, output, session) {
     tags$head(HTML("<title>Local Authority Interactive Tool (LAIT)</title>")),
     tags$head(tags$link(rel = "shortcut icon", href = "dfefavicon.png")),
     tags$head(htmltools::includeScript("www/custom_js.js")),
-    tags$head(
-      tags$script(HTML(js))
-    ),
     shinytitle::use_shiny_title(),
     tags$html(lang = "en"),
     # Add meta description for search engines
