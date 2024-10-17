5452012a3fef61905d90e7c5bbd3fc8f29c8a7e7
Jake Tufts <jake.tufts@education.gov.uk>
2024-10-17 17:30:54 +0100

    Feat: Adding download option to stat neigh multi line chart

--- www/custom_js.js
+++ www/custom_js.js
@@ -5,8 +5,32 @@ async function getImageBlobFromUrl(url) {
   return blob;
 }
 $(document).ready(function () {
-  $("#copybtn").on("click", async () => {
-    const src = $("#stat_n_focus_line-plotDF>img").attr("src");
+  $("#copybtn_focus_line").on("click", async () => {
+    const src = $("#stat_n_focus_line-copy_plot>img").attr("src");
+    try {
+      const blob = await getImageBlobFromUrl(src);
+      await navigator.clipboard.write([
+        new ClipboardItem({
+          [blob.type]: blob
+        })
+      ]);
+      alert("Image copied to clipboard!");
+    } catch (err) {
+      console.error(err.name, err.message);
+      alert("There was an error while copying image to clipboard :/");
+    }
+  });
+});
+
+
+async function getImageBlobFromUrl(url) {
+  const fetchedImageData = await fetch(url);
+  const blob = await fetchedImageData.blob();
+  return blob;
+}
+$(document).ready(function () {
+  $("#copybtn_multi_line").on("click", async () => {
+    const src = $("#stat_n_multi_line-copy_plot>img").attr("src");
     try {
       const blob = await getImageBlobFromUrl(src);
       await navigator.clipboard.write([
