db03bf15a8bafb3a166965cb27c293ef46e169cd
Jake Tufts <jake.tufts@education.gov.uk>
2024-10-18 17:37:44 +0100

    Feat: Updated JS to also work for stat neigh multi line chart

--- www/custom_js.js
+++ www/custom_js.js
@@ -1,12 +1,25 @@
-
 async function getImageBlobFromUrl(url) {
-  const fetchedImageData = await fetch(url);
-  const blob = await fetchedImageData.blob();
-  return blob;
+  try {
+    const fetchedImageData = await fetch(url);
+    if (!fetchedImageData.ok) {
+      throw new Error('Image fetch failed');
+    }
+    const blob = await fetchedImageData.blob();
+    return blob;
+  } catch (error) {
+    console.error('Error fetching image:', error);
+    throw error;
+  }
 }
+
 $(document).ready(function () {
-  $("#stat_n_focus_line-copybtn").on("click", async () => {
-    const src = $("#stat_n_focus_line-copy_plot>img").attr("src");
+  // General function to handle image copying
+  async function copyImage(buttonId, imageContainerId) {
+    const src = $(`#${imageContainerId}>img`).attr("src");
+    if (!src) {
+      alert("No image source found!");
+      return;
+    }
     try {
       const blob = await getImageBlobFromUrl(src);
       await navigator.clipboard.write([
@@ -17,7 +30,17 @@ $(document).ready(function () {
       alert("Image copied to clipboard!");
     } catch (err) {
       console.error(err.name, err.message);
-      alert("There was an error while copying image to clipboard :/");
+      alert("There was an error while copying the image to clipboard :/");
     }
+  }
+
+  // Event listener for "stat_n_focus_line-copybtn"
+  $("#stat_n_focus_line-copybtn").on("click", async () => {
+    await copyImage("stat_n_focus_line-copybtn", "stat_n_focus_line-copy_plot");
+  });
+
+  // Event listener for "stat_n_multi_line-copybtn"
+  $("#stat_n_multi_line-copybtn").on("click", async () => {
+    await copyImage("stat_n_multi_line-copybtn", "stat_n_multi_line-copy_plot");
   });
 });
