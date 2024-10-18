edb0bd2117f779ffd86a60d93fbaf79caac57173
Jake Tufts <jake.tufts@education.gov.uk>
2024-10-18 14:02:15 +0100

    Adding css changes to make cards bigger and updating screenshots

--- www/custom_js.js
+++ www/custom_js.js
@@ -1,11 +1,1 @@
 
-document.addEventListener("copy", (event) => {
-  const anchorNode = document.getSelection().anchorNode;
-  if (anchorNode instanceof HTMLElement &&
-    anchorNode.classList.contains("selectize-input")) {
-    const items = Array.from(anchorNode.getElementsByClassName("item active"));
-    const string = items.map(i => i.innerText).join(", ");
-    event.clipboardData.setData("text/plain", string);
-    event.preventDefault();
-  }
-});
