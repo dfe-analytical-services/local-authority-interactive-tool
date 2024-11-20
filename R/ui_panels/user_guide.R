7ab4fdb41fa7396cd97070bb64714c5ac84c9a3c
Jake Tufts <jake.tufts@education.gov.uk>
2024-11-20 12:54:09 +0000

    Feat: Added LA view to user guide

--- R/ui_panels/user_guide.R
+++ R/ui_panels/user_guide.R
@@ -77,16 +77,113 @@ user_guide_panel <- function() {
               "Create Your Own"
             ),
             descriptions = list(
-              "Local Authority View",
+              tagList(
+                HTML(
+                  "This view begins with a choose local authority (LA), topic
+                    and indicator section.  Use the three dropdowns to select a
+                    LA and measure of choice.
+                   <br>
+                   <br>
+                   As this is an interactive system, the tables and chart (and
+                    relevant titling) change automatically depending on the
+                    indicator chosen.
+                   <br>
+                   <br>
+                   The table beneath the dropdowns will show time series data
+                    for the chosen indicator in respect of the LA, its Region,
+                    Statistical Neighbours and England averages - for
+                    comparison/benchmarking purposes (figure 2).
+                   <br>
+                   <br>
+                   Note that your selected LA is highlighted in light blue
+                    within the data table, and that there is an option to
+                    download the data.
+                   <br>
+                   <br>
+                   The figures presented are simple averages for the selected
+                    LA’s 10 statistical neighbours (SNs).  They provide a simple
+                    comparator of the measure without placing too much emphasis
+                    on any one authority.  Where data does not exist for one or
+                    more of the selected LA’s SN group, the information is
+                    excluded from the mean calculation.
+                   <br>
+                   <br>
+                   <b>Figure 2: Data Table for Chosen Authority/Data Item</b>
+                   <br>"
+                ),
+                tags$img(
+                  src = "images/user_guide/LA_lvl_main_table.png",
+                  alt = "Figure 2: Data Table for Chosen Authority/Data Item",
+                  style = "width:100%; max-width:600px; margin-top:10px;"
+                ),
+                HTML(
+                  "<br>
+                   <br>
+                   <br>
+                   Beneath this is a sub-table showing latest year on year
+                    trend, change from previous year and national rank.
+                    If applicable the authority is placed into a quartile
+                    banding and formatting will flag up upper (A) and Lower (D)
+                    quartiles for clarity.
+                   <br>
+                   <br>
+                   <b>Figure 3: Trends and Quartile bandings</b>
+                   <br>"
+                ),
+                tags$img(
+                  src = "images/user_guide/LA_lvl_stats_table.png",
+                  alt = "Figure 3: Trends and Quartile bandings",
+                  style = "width:100%; max-width:600px; margin-top:10px;"
+                ),
+                HTML(
+                  "<br>
+                   <br>
+                   <br>
+                   This table is followed by a chart (figure 4), which plots the
+                    authority, national, region and statistical neighbour
+                    average data over the same period as the data table,
+                    providing an interactive visual representation. The data is
+                    displayed as a line chart by default but can be changed to
+                    a bar chart, both are downloadable.
+                   <br>
+                   <br>
+                   <b>Figure 4: Charting the Data</b>
+                   <br>"
+                ),
+                tags$img(
+                  src = "images/user_guide/LA_lvl_charts.png",
+                  alt = "Figure 4: Charting the Data",
+                  style = "width:100%; max-width:600px; margin-top:10px;"
+                ),
+                HTML(
+                  "<br>
+                   <br>
+                   <br>
+                   Beneath the chart a description of the data item is provided
+                    (figure 5), as well as an indication of last and next update
+                    dates, alongside a hyperlink to the data source (if clicked,
+                    the source will open in new tab).
+                   <br>
+                   <br>
+                   <b>Figure 5: Data Description</b>
+                   <br>"
+                ),
+                tags$img(
+                  src = "images/user_guide/LA_lvl_metadata.png",
+                  alt = "Figure 5: Data Description",
+                  style = "width:100%; max-width:600px; margin-top:10px;"
+                )
+              ),
               "Regional View",
               "Statistical Neighbour View",
               tagList(
-                HTML(
+                p(
                   "The All LAs view presents all 153 Local Authorities, in
                   alphabetical order, plus previous authorities for those that
                   have recently undergone a local government reorganisation,
-                  (figure 9). The table is downloadable.<br>"
+                  (figure 9). The table is downloadable."
                 ),
+                p(HTML("<b>Figure 9 showing an example table of all Local Authorities.</b>")),
                 tags$img(
                   src = "images/user_guide/all_LAs_view_top_table.png",
                   alt = "Figure 9 showing an example table of all Local Authorities.",
