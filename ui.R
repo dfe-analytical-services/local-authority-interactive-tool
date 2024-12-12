8f60a4689f717c9deaf83893f60dcaba4c0127f4
Jake Tufts <jake.tufts@education.gov.uk>
2024-12-12 12:11:15 +0000

    Fix: Screenshot of charts now working as added shinyWidgets::useShinydashboard() back in

--- ui.R
+++ ui.R
@@ -51,6 +51,7 @@ ui <- function(input, output, session) {
     gap = 0,
 
     # Load javascript dependencies --------------------------------------------
+    shinyWidgets::useShinydashboard(),
     shinyjs::useShinyjs(),
     tags$head(htmltools::includeScript("www/custom_js.js")),
     reactable.extras::reactable_extras_dependency(),
