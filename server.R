489326f04a7ff0844b23efd340b8ea2e8d3f1c15
Jake Tufts <jake.tufts@education.gov.uk>
2024-12-24 10:49:16 +0000

    Feat: Updating cookie_banner_server for new navigation panel id param

--- server.R
+++ server.R
@@ -168,11 +168,13 @@ server <- function(input, output, session) {
   })
 
   # Cookies logic =============================================================
-  output$cookies_status <- cookies_banner_server_jt(
+  # TODO: Delete cookies_banner_server_jt() when dfeshiny corrected
+  output$cookies_status <- dfeshiny::cookies_banner_server(
     input_cookies = shiny::reactive(input$cookies),
     parent_session = session,
     google_analytics_key = google_analytics_key,
-    cookies_link_panel = "cookies_information"
+    cookies_link_panel = "cookies_information",
+    cookies_nav_id = "pages"
   )
 
   dfeshiny::cookies_panel_server(
