244771b3d6613cee26de6f9c886bde27f7022ccf
Rich Bielby <richbielby@e.email>
2026-09-16 16:53:09 +0100

    Stepping up timeouts on UI tests and removed cat terminal output on load

--- tests/testthat/test-UI-mod_la_lvl_table.R
+++ tests/testthat/test-UI-mod_la_lvl_table.R
@@ -34,8 +34,8 @@
 testthat::test_that("There are 4 rows in the LA main table", {
   shinytest_app <- shinytest2::AppDriver$new(
     app_dir = here::here("tests/testthat/test-app_mod_la_lvl_table/"),
-    load_timeout = 45 * 1000,
-    timeout = 60 * 1000,
+    load_timeout = 120 * 1000,
+    timeout = 120 * 1000,
     wait = TRUE
   )
 
